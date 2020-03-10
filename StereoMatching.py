12422da90a3587d3c98fe516518ac53fd5a173ca
chaozy <chaozy.zhu.19@ucl.ac.uk>
2020-03-10 10:41:21 +0000

    finish all

--- StereoMatching.py
+++ StereoMatching.py
@@ -1,31 +1,32 @@
 from RandomMatch import drawRandom
 from PIL import Image
 import numpy as np
-from matplotlib import pyplot as PLT
 import time
 
 def matchCost(left, right):
-    z = (int(left) + int(right)) / 2
+    z = ((left) + (right)) / 2
     result = ((z - left) * (z - right)) / 16
     return abs(result)
 
 def disparity(left, right):
     return abs(left - right)
 
-def costMatrix(left, right, rowSize):
-    costOfOcclusion = 3.8
-    colSize = len(left)
-    arr = [[0 for i in range(colSize + 1)] for i in range(rowSize + 1)]
+def rgbToIntensity(rgb):
+    return sum(rgb) / len(rgb)
+
+def costMatrix(left, right):
+    costOfOcclusion = 1.8
+    size = len(left)
+    arr = [[0 for i in range(size + 1)] for i in range(size + 1)]
     steps = []
-    for i in range(1, rowSize):
+    test = [None for _ in range(size + 1)]
+    for i in range(1, size + 1):
         arr[i][0] = i * costOfOcclusion
-    for i in range(1, colSize):
         arr[0][i] = i * costOfOcclusion
 
-    for i in range(1, rowSize):
+    for i in range(1, size):
         currentSteps = []
-        for j in range(1, colSize):
-            #print(left[i], " ", right[j], "  " , matchCost(left[i], right[j]))
+        for j in range(1, size):
             costOfMatch = matchCost(left[i], right[j]) + arr[i - 1][j - 1]
             leftOcculsion = arr[i-1][j] + costOfOcclusion
             rightOcculsion = arr[i][j - 1] + costOfOcclusion
@@ -46,54 +47,45 @@ def backward(decisions):
     result = [0 for i in range(len(decisions) + 1)]
     while (i >= 0 and j >= 0):
         last_decision = decisions[i][j]
-        #print(last_decision)
         if last_decision == 1: # matched case
-            #if i != j:
-            result[i] = result[j] = disparity(i, j) + 128
+            result[i] = disparity(i, j) + 128
             i -= 1
             j -= 1
-
         elif last_decision == 3 : # left[i] is occuluded
-            result[i] = 0
+            # result[i] = 0
             i -= 1
-        else:                                 # right[j] is occuluded
-            result[j] = 0
+        else:                     # right[j] is occuluded
             j -= 1
+    #print(result)
     return result
 
 def go():
     # bigSize = 512
     # smallSize = 256
     # drawRandom(bigSize, smallSize, 128, 124, 132)
-    imgA = Image.open("/Users/chaozy/Desktop/CS/Algorithm/Coursework2/Stereo Pairs/Pair 2/disp1.png")
+    imgA = Image.open("/Users/chaozy/Desktop/CS/Algorithm/Coursework2/Stereo Pairs/Pair 2/view1.png")
     #imgA = Image.open("imgLeft.png")
     imgA = np.asarray(imgA)
-    imgB = Image.open("/Users/chaozy/Desktop/CS/Algorithm/Coursework2/Stereo Pairs/Pair 2/disp2.png")
+    imgA = np.array([[rgbToIntensity(rgb) for rgb in rows] for rows in imgA])
+    imgB = Image.open("/Users/chaozy/Desktop/CS/Algorithm/Coursework2/Stereo Pairs/Pair 2/view2.png")
     #imgB = Image.open("imgRight.png")
     imgB = np.asarray(imgB)
+    imgB = np.array([[rgbToIntensity(rgb) for rgb in rows] for rows in imgB])
 
     bigSize = len(imgA)
     smallSize = len(imgA[0])
-    disparityMatrix = np.array([[0 for i in range(bigSize)] for i in range(smallSize)])
+    disparityMatrix = np.array([[0 for i in range(smallSize)] for i in range(bigSize)])
+    #disparityMatrix = np.array([[0 for i in range(bigSize)] for i in range(bigSize)])
 
-    for i in range(len(imgA)):
-        #if np.any(imgA[i] != imgB[i]):
+    for i in range(bigSize):
             print(i)
-            #print(imgA[i][90:], imgB[i][90:])
-            disparityMatrix[i] = backward(costMatrix(imgA[i], imgB[i], bigSize))
+            disparityMatrix[i] = backward(costMatrix(imgA[i], imgB[i]))
 
-    print(disparityMatrix.shape)
-    # img = Image.new('L', (bigSize, bigSize))
-    # img.putdata(np.reshape(disparityMatrix, bigSize * bigSize))
-    # disparityMatrix = np.ndarray.flatten(disparityMatrix)
-
-    img = Image.new('L', (bigSize, smallSize))
+    img = Image.new('L', (smallSize, bigSize))
     img.putdata(np.reshape(disparityMatrix, smallSize * bigSize))
 
-    img.save("result2.png")
+    #img.save("result1.png")
     img.show()
-    # PLT.imshow(disparityMatrix)
-    # PLT.show()
 
 t1 = time.time()
 go()
